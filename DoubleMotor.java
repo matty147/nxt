1ba28a4439fb9ff43af141a876f9e6a3896ecb24
matty147 <88089429+matty147@users.noreply.github.com>
2022-03-01 23:03:58 +0100

    spaces

--- DoubleMotor.java
+++ DoubleMotor.java
@@ -2,122 +2,122 @@ import lejos.robotics.RegulatedMotorListener;
 import lejos.robotics.RegulatedMotor;
 
 public class DoubleMotor implements RegulatedMotor {
-	private final RegulatedMotor left;
-	private final RegulatedMotor right;
+    private final RegulatedMotor left;
+    private final RegulatedMotor right;
 
-	public DoubleMotor(RegulatedMotor left, RegulatedMotor right) {
-		this.left = left;
-		this.right = right;
-	}
+    public DoubleMotor(RegulatedMotor left, RegulatedMotor right) {
+        this.left = left;
+        this.right = right;
+    }
 
-	public void forward() {
-		left.forward();
-		right.forward();
-	}
+    public void forward() {
+        left.forward();
+        right.forward();
+    }
 
-	public void backward() {
-		left.backward();
-		right.backward();
-	}
+    public void backward() {
+        left.backward();
+        right.backward();
+    }
 
-	public void stop() {
-		left.stop();
-		right.stop();
-	}
+    public void stop() {
+        left.stop();
+        right.stop();
+    }
 
-	public void flt() {
-		left.flt();
-		right.flt();
-	}
+    public void flt() {
+        left.flt();
+        right.flt();
+    }
 
-	public boolean isMoving() {
-		return left.isMoving();
-	}
-	
+    public boolean isMoving() {
+        return left.isMoving();
+    }
+    
 
-	public void addListener(RegulatedMotorListener listener) {
-	}
+    public void addListener(RegulatedMotorListener listener) {
+    }
 
-	public RegulatedMotorListener removeListener() {
-		return null;
-	}
+    public RegulatedMotorListener removeListener() {
+        return null;
+    }
 
-	public void stop(boolean immediateReturn) {
-		left.stop(true);
-		right.stop(immediateReturn);
-	}
+    public void stop(boolean immediateReturn) {
+        left.stop(true);
+        right.stop(immediateReturn);
+    }
 
-	public void flt(boolean immediateReturn) {
-		left.flt(true);
-		right.flt(immediateReturn);
-	}
+    public void flt(boolean immediateReturn) {
+        left.flt(true);
+        right.flt(immediateReturn);
+    }
 
-	public void waitComplete() {
-		left.waitComplete();
-		right.waitComplete();
-	}
+    public void waitComplete() {
+        left.waitComplete();
+        right.waitComplete();
+    }
 
-	public void rotate(int angle, boolean immediateReturn) {
-		left.rotate(angle, true);
-		right.rotate(angle, immediateReturn);
-	}
+    public void rotate(int angle, boolean immediateReturn) {
+        left.rotate(angle, true);
+        right.rotate(angle, immediateReturn);
+    }
 
-	public void rotate(int angle) {
-		left.rotate(angle, true);
-		right.rotate(angle);
-	}
+    public void rotate(int angle) {
+        left.rotate(angle, true);
+        right.rotate(angle);
+    }
 
-	public void rotateTo(int limitAngle) {
-		left.rotateTo(limitAngle);
-		right.rotateTo(limitAngle);
-	}
+    public void rotateTo(int limitAngle) {
+        left.rotateTo(limitAngle);
+        right.rotateTo(limitAngle);
+    }
 
-	public void rotateTo(int limitAngle, boolean immediateReturn) {
-		left.rotateTo(limitAngle, true);
-		right.rotateTo(limitAngle, immediateReturn);
-	}
+    public void rotateTo(int limitAngle, boolean immediateReturn) {
+        left.rotateTo(limitAngle, true);
+        right.rotateTo(limitAngle, immediateReturn);
+    }
 
-	public int getLimitAngle() {
-		return left.getLimitAngle();
-	}
+    public int getLimitAngle() {
+        return left.getLimitAngle();
+    }
 
-	public void setSpeed(int speed) {
-		left.setSpeed(speed);
-		right.setSpeed(speed);
-	}
+    public void setSpeed(int speed) {
+        left.setSpeed(speed);
+        right.setSpeed(speed);
+    }
 
-	public int getSpeed() {
-		return left.getSpeed();
-	}
+    public int getSpeed() {
+        return left.getSpeed();
+    }
 
-	public float getMaxSpeed() {
-		return left.getMaxSpeed();
-	}
+    public float getMaxSpeed() {
+        return left.getMaxSpeed();
+    }
 
-	public boolean isStalled() {
-		return left.isStalled() || right.isStalled();
-	}
+    public boolean isStalled() {
+        return left.isStalled() || right.isStalled();
+    }
 
-	public void setStallThreshold(int error, int time) {
-		left.setStallThreshold(error, time);
-		right.setStallThreshold(error, time);
-	}
+    public void setStallThreshold(int error, int time) {
+        left.setStallThreshold(error, time);
+        right.setStallThreshold(error, time);
+    }
 
-	public void setAcceleration(int acceleration) {
-		left.setAcceleration(acceleration);
-		right.setAcceleration(acceleration);
-	}
-	
-	public int getRotationSpeed() {
-		return left.getRotationSpeed();
-	}
-	 
-	public int getTachoCount() {
-		return left.getTachoCount();
-	}
+    public void setAcceleration(int acceleration) {
+        left.setAcceleration(acceleration);
+        right.setAcceleration(acceleration);
+    }
+    
+    public int getRotationSpeed() {
+        return left.getRotationSpeed();
+    }
+     
+    public int getTachoCount() {
+        return left.getTachoCount();
+    }
 
-	public void resetTachoCount() {
-		left.resetTachoCount();
-		right.resetTachoCount();
-	}
+    public void resetTachoCount() {
+        left.resetTachoCount();
+        right.resetTachoCount();
+    }
 }
--- Roomba.java
+++ Roomba.java
@@ -15,78 +15,78 @@ import lejos.robotics.objectdetection.FeatureListener;
 import lejos.robotics.objectdetection.RangeFeatureDetector;
 
 public class Roomba implements FeatureListener {
-	private final static double WHEEL_DIAMETER = 13.5;
-	
+    private final static double WHEEL_DIAMETER = 13.5;
+    
     private final RegulatedMotor mainMotor;
-	private final RegulatedMotor steeringMotor;
-	private final FeatureDetector fd;
+    private final RegulatedMotor steeringMotor;
+    private final FeatureDetector fd;
     private final Random rand = new Random();
-	
+    
     public Roomba(final RegulatedMotor mainMotor, final RegulatedMotor steeringMotor, final SensorPort sonicPort) {
-		this.mainMotor = mainMotor;
-		this.steeringMotor = steeringMotor;
-		
-		RangeFinder sonar = new UltrasonicSensor(sonicPort);
+        this.mainMotor = mainMotor;
+        this.steeringMotor = steeringMotor;
+        
+        RangeFinder sonar = new UltrasonicSensor(sonicPort);
         fd = new RangeFeatureDetector(sonar, 40, 250);
         fd.enableDetection(false); 
         fd.addListener(this);
     }
-	
-	public void enableDetection(boolean enable) {
-        fd.enableDetection(enable); 
-	}		
     
-	public void travel(double distance, boolean immediateReturn) {
-		int tachos = (int)((distance * 360) / (WHEEL_DIAMETER * Math.PI));
-		if (tachos > 0) {
-			System.out.println("Forward T");
-		}
-		else {
-			System.out.println("Backward T");
-		}
-			
-		mainMotor.rotate(-tachos, immediateReturn);	   
-	}
+    public void enableDetection(boolean enable) {
+        fd.enableDetection(enable); 
+    }       
+    
+    public void travel(double distance, boolean immediateReturn) {
+        int tachos = (int)((distance * 360) / (WHEEL_DIAMETER * Math.PI));
+        if (tachos > 0) {
+            System.out.println("Forward T");
+        }
+        else {
+            System.out.println("Backward T");
+        }
+            
+        mainMotor.rotate(-tachos, immediateReturn);    
+    }
 
-	public void forward() {
-		System.out.println("Forward");
-		mainMotor.backward();	   
-	}
-	   
-	public void featureDetected(Feature feature, FeatureDetector detector) {
-		Sound.beepSequence();
+    public void forward() {
+        System.out.println("Forward");
+        mainMotor.backward();      
+    }
+       
+    public void featureDetected(Feature feature, FeatureDetector detector) {
+        Sound.beepSequence();
 
-		detector.enableDetection(false);
+        detector.enableDetection(false);
 
-		travel(-(5 + rand.nextInt(10)), false);
-	
-		// turn a wheels by a random angle
-		int angle = (Math.random() > 0.5 ? -1 : 1) * (10 + rand.nextInt(50));   // todo can be too big
-		System.out.println("Angle: " + angle);
-		steeringMotor.rotate(angle);
+        travel(-(5 + rand.nextInt(10)), false);
+    
+        // turn a wheels by a random angle
+        int angle = (Math.random() > 0.5 ? -1 : 1) * (10 + rand.nextInt(50));   // todo can be too big
+        System.out.println("Angle: " + angle);
+        steeringMotor.rotate(angle);
 
-	    detector.enableDetection(true);
+        detector.enableDetection(true);
 
-	    forward();
-	}
+        forward();
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-		final DoubleMotor mainMotor = new DoubleMotor(Motor.B, Motor.C);
-		mainMotor.setSpeed(72);
+        final DoubleMotor mainMotor = new DoubleMotor(Motor.B, Motor.C);
+        mainMotor.setSpeed(72);
 
-		final RegulatedMotor steeringMotor = Motor.A;
-    	
+        final RegulatedMotor steeringMotor = Motor.A;
+        
         final Roomba roomba = new Roomba(mainMotor, steeringMotor, SensorPort.S4);
-		
+        
         while(!Button.ESCAPE.isDown()) {
-        	System.out.println("Press ENTER key");
-        	Button.ENTER.waitForPressAndRelease();
-        	
-        	roomba.enableDetection(true); 
-	        roomba.forward();
-	        
-	        while(mainMotor.isMoving())
-	        	Thread.sleep(500);
+            System.out.println("Press ENTER key");
+            Button.ENTER.waitForPressAndRelease();
+            
+            roomba.enableDetection(true); 
+            roomba.forward();
+            
+            while(mainMotor.isMoving())
+                Thread.sleep(500);
         }
     }
 
